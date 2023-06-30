349faca4fa16eb1a1db08d5ba15ef70b45fdef67
Sofia Hudson <sofia-sama@hotmail.com>
2023-06-30 12:31:37 +0200

    Add file middleware to book router

--- src/routers/book.router.ts
+++ src/routers/book.router.ts
@@ -3,26 +3,32 @@ import { BookController } from '../controllers/book.controller.js';
 import { BookRepo } from '../repository/book.mongo.repository.js';
 import { Repository } from '../repository/repository.js';
 import { Book } from '../entities/book.js';
-
-import createDebug from 'debug';
+import { User } from '../entities/user.js';
 import { AuthInterceptor } from '../middleware/auth.interceptor.js';
 import { UserRepo } from '../repository/user.mongo.repository.js';
+import { FileMiddleware } from '../middleware/files.js';
+
+import createDebug from 'debug';
 const debug = createDebug('W6:BookRouter');
 
 debug('Executed');
 
-const repo: Repository<Book> = new BookRepo();
-const userRepo = new UserRepo();
-const controller = new BookController(repo, userRepo);
-const auth = new AuthInterceptor(repo);
+const bookRepo: Repository<Book> = new BookRepo();
+const userRepo: Repository<User> = new UserRepo();
+const controller = new BookController(bookRepo, userRepo);
+const auth = new AuthInterceptor(bookRepo);
 export const bookRouter = createRouter();
+const fileStore = new FileMiddleware();
 
 bookRouter.get('/book', controller.getAll.bind(controller));
 bookRouter.get('/book/:id', controller.getById.bind(controller));
 
 bookRouter.post(
   '/user/book',
+  fileStore.singleFileStore('image').bind(fileStore),
   auth.logged.bind(auth),
+  fileStore.optimization.bind(fileStore),
+  fileStore.saveImage.bind(fileStore),
   controller.post.bind(controller)
 );
 bookRouter.patch(
